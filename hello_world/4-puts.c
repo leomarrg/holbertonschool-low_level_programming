aa53a357039fffcc0f36f141ff44d938064aef35
leomar <leomar.rodriguez1@upr.edu>
2023-10-13 14:55:18 -0400

    testing new comment description for betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,10 +4,16 @@
  * function_main: main function it hold the code to print what the programs ask
  *@parameterx: void parameter
  *
- *this description will print: "Programming is like building a multilingual puzzle"
+ *this will print: "Programming is like building a multilingual puzzle"
  *header is stdio.h
  *this returns 0 if code works
 */
+
+/*main - The main function
+ *
+ *
+*/
+
 
 int main(void)
 {
