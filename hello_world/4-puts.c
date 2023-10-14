7fc1908f7f89a7bfad8056a44d2840382c82d6c1
leomar <leomar.rodriguez1@upr.edu>
2023-10-14 11:59:29 -0400

    testing comment description for return value

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-/*
- * function_main: main function it hold the code to print what the programs ask
- *@parameterx: void parameter
+/**
+ *main - main function it hold the code to print what the programs ask
  *
- *this will print: "Programming is like building a multilingual puzzle"
  *header is stdio.h
- *this returns 0 if code works
+ *return: 0 if code works
 */
-
-/*main - The main function
- *
- *
-*/
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
