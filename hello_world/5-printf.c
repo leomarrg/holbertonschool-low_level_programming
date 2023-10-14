fc398b54bfb9bf23dc0163479018dc5cff6b00a3
leomar <leomar.rodriguez1@upr.edu>
2023-10-14 12:31:27 -0400

    made file exe, added betty coding style and printf to print whats asked

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - holds code to be printed
+ * Description: will print what's asked
+ * Return: will return 0 if succesful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome if a piece of art,\n");
+	return (0);
+}
