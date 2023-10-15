8e41149e5595644bd8caddf1006f815ec2b4a189
leomar <leomar.rodriguez1@upr.edu>
2023-10-15 13:23:18 -0400

    had syntax error in a line after dpouble quotes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
 int main(void)
 {	
 	printf("Size of a char: %zu bytes\n", sizeof(char));
-	printf("Size of an int: %zu bytes\n"\n, sizeof(int));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long int: %zu bytes\n", sizeof(long int));
 	printf("Size of a long long int: bytes %zu\n" sizeof(long long int));
 	printf("Size of a float: %zu\n" sizeof(float));
