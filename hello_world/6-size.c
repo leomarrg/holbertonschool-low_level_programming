e69dd46ab770c176bc0c4031a1ffd26c5bf1b0f7
leomar <leomar.rodriguez1@upr.edu>
2023-10-15 13:36:01 -0400

    trying to output on 32 and 64 machine

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: byte(s) %zu\n", sizeof(long long int));
-	printf("Size of a float: bytes(s) %zu\n", sizeof(float));
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1d bytes(s)\n", sizeof(float));
 	return (0);
 }
