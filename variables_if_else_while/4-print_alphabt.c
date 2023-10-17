5570db48081dd9502bcaf9be88d1d3e9a17a5a3c
leomar <leomar.rodriguez1@upr.edu>
2023-10-17 03:41:23 -0400

    made file, use putchar inside for loop to print whole alphabet with a new line at the end

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that it will do something
+ * Description: this function will do something with char
+ * Return: if 0 it runs succesfully
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
