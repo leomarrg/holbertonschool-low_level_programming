c71e1ad326c2432295168c94f06479b45f9c75fd
leomar <leomar.rodriguez1@upr.edu>
2023-10-17 13:32:36 -0400

    this program will output _putchar on screen

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this main function contains everything
+ * Description: this will print _putchar
+ * Return: if succesfull will return 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
