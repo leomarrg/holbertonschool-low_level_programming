fb9320cb34cdd86b671b675a0a4b184db6b77f7a
leomar <leomar.rodriguez1@upr.edu>
2023-10-17 11:29:33 -0400

    printing all combinations of single digits numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that it will do something
+ * Description: this function will do something with char
+ * Return: if 0 it runs succesfully
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+		if (c < '9')
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
