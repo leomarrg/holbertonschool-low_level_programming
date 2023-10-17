4caad2b7d3a2635b9983aee39e6369f032973cad
leomar <leomar.rodriguez1@upr.edu>
2023-10-17 10:13:32 -0400

    printed numbers from 0-9 using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function that it will do something
+ * Description: this function will do something with char
+ * Return: if 0 it runs succesfully
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c % 10 +'0');
+	}
+	putchar('\n');
+	return (0);
+}
