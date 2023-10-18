848fedab76f6c3ee3f136cb2568b9df663471e8f
leomar <leomar.rodriguez1@upr.edu>
2023-10-18 09:20:53 -0400

    created function that prints the signs of the numbers

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- *print_sign - will check n
- *@(n): the char to be checked
- *Return: 1 if positive, 0 if zero, -1 if negative
+ * print_sign - will check n
+ * @n: the char to be checked
+ * Return: 1 if positive, 0 if zero, -1 if negative
 */
-
- int print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
