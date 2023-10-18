fe6fb54e883bd50abd430f12ffbbfd4aec3d50b1
leomar <leomar.rodriguez1@upr.edu>
2023-10-18 09:55:25 -0400

    created funciton that returns the lastdigit of numbers

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - will find the last digit of the given number
+ * @n: the char to be checked
+ * Return: will return the last digit of given numbers
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
