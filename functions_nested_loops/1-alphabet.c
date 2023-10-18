556b8af6f729ac6648b5c5b44e036f8b958f6654
leomar <leomar.rodriguez1@upr.edu>
2023-10-17 22:48:42 -0400

    added prototype to main.h and created function that prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_alphabetm- this funciton will print the alphabet
+ *it doesnt return nothing
+ *
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
