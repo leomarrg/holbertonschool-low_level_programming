0c625bab1fc1c8b585c53affd8b129b6cb3a7805
leomar <leomar.rodriguez1@upr.edu>
2023-10-18 14:36:47 -0400

    created function that prints every second of a day in military time

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - function that prints the album
+ * Return: it returns nothing
+*/
+
+void jack_bauer(void)
+{
+	int startTimeSecs, endTimeSecs;
+	int startTimeHour, endTimeHour;
+	endTimeSecs = 60;
+	endTimeHour = 24;
+
+	for (startTimeHour = 0; startTimeHour < endTimeHour; startTimeHour++)
+		for (startTimeSecs = 0; startTimeSecs < endTimeSecs; startTimeSecs++)
+		{
+			_putchar((startTimeHour / 10) + '0');
+			_putchar((startTimeHour % 10) + '0');
+			_putchar(':');
+			_putchar((startTimeSecs / 10) + '0');
+			_putchar((startTimeSecs % 10) + '0');
+			_putchar('\n');
+		}
+
+}
