32233ca446a2e3b2b47c199a57b666aa330682ca
leomar <leomar.rodriguez1@upr.edu>
2023-10-19 12:02:04 -0400

    fixed betty style errors, created a function that checks all possible outcomes and prints the natural numbers

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - this function will print natural numbers from n to 98
  * @n: will be checked
@@ -11,18 +10,41 @@ void print_to_98(int n)
 	int i;
 
 	if (n < 0)
-		n = -n;
-
-	for (i = n; i <= 98; i++)
 	{
-		printf("%i", i);
-
-		if (i != 98)
+		n = -n;
+		for (i = n; i <= 98; i++)
 		{
-			printf(",");
-			printf(" ");
+			printf("%i", i);
+			if (i != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
 		}
 	}
-
+	if (n > 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			if (i != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
 	printf("\n");
 }
