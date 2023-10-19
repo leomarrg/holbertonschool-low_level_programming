2bf738232d9e947f7c41c2dbaee309bee9e8d1e9
leomar <leomar.rodriguez1@upr.edu>
2023-10-19 10:41:38 -0400

    created function that adds to integer together

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - this function returns the sum of to integers
+ * @a: this parameter will be added
+ * @b: this parameter will be added
+ * Return: will return the add of a and b
+*/
+int add(int a, int b)
+{
+	a = 5;
+	b = 5;
+
+	return a + b;
+}
+
+
+
+
+
+
+
