5515505b9ed17c09e6a06225435333cfd382e122
leomar <leomar.rodriguez1@upr.edu>
2023-10-20 14:20:19 -0400

    fixed betty cody style errors

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _isdigit - checks if c is number
+ * @c: parameter to be checked
+ * Return: either 1 or 0
+*/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '0')
