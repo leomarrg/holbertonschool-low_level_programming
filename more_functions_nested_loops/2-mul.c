f3d9644908989f2656d67d88f5d37911c1ce3aee
leomar <leomar.rodriguez1@upr.edu>
2023-10-20 14:36:23 -0400

    fixed errors in betty coding style

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - will multiply 2 integers
+ * @a:one integer
+ * @b: another integer
+ * Return: will return the product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
