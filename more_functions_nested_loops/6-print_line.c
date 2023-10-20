8dce860e6ef632e6f9289c60fd433b4eb776a95b
leomar <leomar.rodriguez1@upr.edu>
2023-10-20 15:56:12 -0400

    added conditions to treat every case

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,17 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	if (n < 0 || n == 0)
+	{
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
