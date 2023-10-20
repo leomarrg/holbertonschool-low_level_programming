ae79df0a292513fc364b5b5f7fc0bb9c08499b26
leomar <leomar.rodriguez1@upr.edu>
2023-10-20 14:09:58 -0400

    had error in main.h, missing ; at end of prototype

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _isupper - this function checks if is upper case or lower case
+ * @c: char that is verified
+ * Return: returns 0 if lower case 1 is uppercase
+*/
+
 int _isupper(int c)
 {
-	if (c >= 'A'&& c <= 'Z')
-		return 1;
-	else
-		return 0;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else 
+		return (0);
+
 }
