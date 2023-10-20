95fc478b2e6e4a1df76b664a6f15593dc53e9f24
leomar <leomar.rodriguez1@upr.edu>
2023-10-20 14:31:55 -0400

    fixed error in in if was comparing 0 and 0

--- more_functions_nested_loops/1-main.c
+++ more_functions_nested_loops/1-main.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
+	for (c = '0'; c <= '9'; ++c)
+		printf("%c: %d\n", c, _isdigit(c));
+	return (0);
 }
