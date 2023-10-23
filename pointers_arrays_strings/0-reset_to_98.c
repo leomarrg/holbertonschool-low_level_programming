cfcf23e1d4469e00d9ecb717b3bce572f4b33708
leomar <leomar.rodriguez1@upr.edu>
2023-10-23 10:43:19 -0400

    changed

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98(int *n) - function that resets to 98
+ * @n: parameter that takes value
+ * Return: doesn't return anything is a void function
+*/
+
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
