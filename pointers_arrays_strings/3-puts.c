8ffdf9d5460093e5bade739751ffa6f74d7d7503
leomar <leomar.rodriguez1@upr.edu>
2023-10-23 14:16:36 -0400

    fixed

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void _puts - this function prints the string
+ * _puts - this function prints the string
  * @str: receives the string
  * Return: doesn't return anything
 */
@@ -9,14 +9,13 @@
 void _puts(char *str)
 {
 	int i;
+
 	i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-
 	_putchar('\n');
-
 }
