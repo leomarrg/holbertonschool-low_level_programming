6f0e26291aefbf160b840280b9227d4619435a4f
leomar <leomar.rodriguez1@upr.edu>
2023-10-23 13:22:49 -0400

    testing

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that prints the size of a string
+ * @s: parameter that receives the string
+ * Return: it returns the size of the string
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
