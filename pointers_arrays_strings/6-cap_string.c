0cd8fce64c77deb888c5fa2682a654a7a23dca03
leomar <leomar.rodriguez1@upr.edu>
2023-10-27 13:48:11 -0400

    made function that capitalizes the first letter of a word in the string even with separators

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * cap_string - this functino capitalizes all words of a string
  * @str: receives the string
- * Re
+ * Return: returns the string with the first letter capitalized
+*/
+
+char *cap_string(char *str)
+{
+	int isInsideWord;
+	int i;
+
+	isInsideWord = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' ||
+			str[i] == ',' || str[i] == '.' || str[i] == '!' || str[i] == '?')
+		{
+			isInsideWord = 0;
+		}
+		else
+		{
+			if (isInsideWord == 0)
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - ('a' - 'A');
+				}
+			}
+			isInsideWord = 1;
+		}
+	}
+	return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,3 +17,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 int _strcmp(char *s1, char *s2);
+char *cap_string(char *str);
