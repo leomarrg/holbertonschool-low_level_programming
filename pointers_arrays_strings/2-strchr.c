95eb5df010b873e04f2fd8dc36b9fd0c95578120
leomar <leomar.rodriguez1@upr.edu>
2023-10-28 09:36:26 -0400

    makes function that finds a char in a string an returns it first occurrence

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * _strchr - function that locates a char in a string
+ * @s: destination pointer
+ * @c: source pointer
+ * Return: returns the char located in the string
+*/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+	s++;
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,3 +20,4 @@ int _strcmp(char *s1, char *s2);
 char *cap_string(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
