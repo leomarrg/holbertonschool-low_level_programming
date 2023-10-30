b2dc6acfc6e43f333c234492373601c8a5baa953
leomar <leomar.rodriguez1@upr.edu>
2023-10-29 23:04:23 -0400

    fixed errors in betty style

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,15 @@
 
 void print_diagsums(int *a, int size)
 {
-int i;
-    int temp1 = 0;
-    int temp2 = 0;
+	int i;
+	int temp1 = 0;
+	int temp2 = 0;
 
-    for (i = 0; i < size; i++) {
-        temp1 += a[i * size + i];
-        temp2 += a[i * size + (size - 1 - i)];
-    }
+	for (i = 0; i < size; i++)
+	{
+		temp1 += a[i * size + i];
+		temp2 += a[i * size + (size - 1 - i)];
+	}
 
-    printf("%d, %d\n", temp1, temp2);
+	printf("%d, %d\n", temp1, temp2);
 }
