dfa086318987b8bd94c7a4cdc39e8c232ec9c82e
leomar <leomar.rodriguez1@upr.edu>
2023-10-29 23:16:40 -0400

    made changed to handle null terminator in string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,27 +13,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
 	}
-
 	return (NULL);
 }
-/*
-char *_strchr(char *s, char c)
-{
-	int index;
-
-	for (index = 0; s[index] >= '\0'; index++)
-	{
-		if (s[index] == c)
-			return (s + index);
-	}
-
-	return ('\0');
-}
-*/
