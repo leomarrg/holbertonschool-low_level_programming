e59ff40495c569855e5bc2f94ca585d8d99ad2c5
leomar <leomar.rodriguez1@upr.edu>
2023-11-01 23:04:23 -0400

    fixed errors in betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursoin - calculates the value of x raised to the power of y
+ * _pow_recursion - calculates the value of x raised to the power of y
  * @x: a number received by main
  * @y: value to raise to the power
  * Return: returns the calculation of the values received
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y > 0)
 	{
-		return (x * _pow_recursion(x, y -1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else
 	{
