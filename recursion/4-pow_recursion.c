437d41f0a69db7f67294513480bb323237726b75
leomar <leomar.rodriguez1@upr.edu>
2023-11-01 23:01:49 -0400

    calculates the pow of x base to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursoin - calculates the value of x raised to the power of y
+ * @x: a number received by main
+ * @y: value to raise to the power
+ * Return: returns the calculation of the values received
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y -1));
+	}
+	else
+	{
+		return (1);
+	}
+}
