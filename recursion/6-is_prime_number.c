94d22ababfea89fe54200c0d710c4338d2b1c097
leomar <leomar.rodriguez1@upr.edu>
2023-11-05 17:02:38 -0400

    function that verifies if num is prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * findPrime - function that verifies if number is prime
+ * @n: receives the num from main
+ * @factor: var that will find factor
+ * Return: returns 1 if a prime num is found
+*/
+
+int findPrime(int n, int factor)
+{
+	if(factor * factor > n)
+	{
+		return (1);
+	}
+	if (n % factor == 0)
+	{
+		return (0);
+	}
+	return (findPrime(n, factor + 1));
+}
+
+/**
+ * is_prime_number - function that receives number
+ * @n: receives number from main
+ * Return: returns 0 if number is not prime
+*/
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	return (findPrime(n, 2));
+}
