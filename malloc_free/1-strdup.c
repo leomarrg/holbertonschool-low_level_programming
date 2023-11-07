67d2b7467ebb8991b1c58002d77f48be6faf4abf
leomar <leomar.rodriguez1@upr.edu>
2023-11-07 16:14:05 -0400

    function that creates duplicate of string received by main

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a duplicate of a string
+ * @str: receives string from main
+ * Return: returns a copy of the string
+*/
+
+char *_strdup(char *str)
+{
+
+	int i, charCounter;
+	char *strCpy;
+	
+	charCounter = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		charCounter++;
+		str++;
+	}
+
+	strCpy = (char *)malloc(charCounter * sizeof(char));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		strCpy[i] = str[i];
+	}
+	return (strCpy);
+}
