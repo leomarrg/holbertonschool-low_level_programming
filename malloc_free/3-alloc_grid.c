6ddd3926f34895268f4b7f1e02e57d2bc21bbe93
leomar <leomar.rodriguez1@upr.edu>
2023-11-09 10:59:04 -0400

    funciton that initializes an 2d array to 0 and returns it, if height or width are less than 0 return null

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2d array
+ * @width: width of array
+ * @height: of array
+ * Return: returns a 2d array
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+	int **grid, i, j;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+
+	grid = (int **) malloc(width * sizeof(int*));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = (int *) malloc(height * sizeof(int));
+
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j ++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
+
+
