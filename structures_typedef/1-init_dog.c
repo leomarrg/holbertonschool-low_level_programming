4d60cd660f66d56550470deb713cb815f362905c
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 03:49:36 -0400

    added malloc function

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,6 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	*d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (NULL);
+
 	if (d != NULL)
 	{
 		d->name = name;
