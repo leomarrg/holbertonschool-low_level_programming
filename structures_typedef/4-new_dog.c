92add286c2921642590cdc6fd5a4cade953efc23
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 14:14:37 -0400

    function that creates new object and stores name and owner of dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,30 +17,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	while (*name != '\0')
+
+	nameLen = 0;
+	ownerLen = 0;
+
+	while (name[nameLen] != '\0')
 	{
 		nameLen++;
-		name++;
 	}
-	while (*owner != '\0')
+	while (owner[ownerLen] != '\0')
 	{
 		ownerLen++;
-		owner++;
 	}
 	new_dog->name = (char *) malloc(sizeof(char) * (nameLen + 1));
 	new_dog->owner = (char *) malloc(sizeof(char) * (ownerLen + 1));
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
 	for (i = 0; i < nameLen; i++)
 	{
 		new_dog->name[i] = name[i];
 	}
+	new_dog->name[nameLen] = '\0';
 	new_dog->age = age;
 	for (i = 0; i < ownerLen; i++)
 	{
 		new_dog->owner[i] = owner[i];
 	}
+	new_dog->owner[ownerLen] = '\0';
 	return (new_dog);
 }
