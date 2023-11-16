f730ed3c4e6a4ff90658683a32d8a72f495868c8
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 00:57:23 -0400

    defined struct with its attributes

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is the struct def of dog
+ * @name: name received from main
+ * @owner: owner received from main
+ * @age: received from main
+*/
+struct dog
+{
+    char *name;
+    char *owner;
+    float age;
+};
+
+#endif
