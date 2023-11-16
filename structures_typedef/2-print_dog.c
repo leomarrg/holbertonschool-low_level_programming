1e773581e9177d79e86c31c500cb0a4703519cb9
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 13:17:04 -0400

    fixed error in printing name, and in condition of age to include 0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name != NULL)
-		printf("Name:%s\n", d->name);
+		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
 
-	if (d->age > 0)
+	if (d->age >= 0)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)\n");
