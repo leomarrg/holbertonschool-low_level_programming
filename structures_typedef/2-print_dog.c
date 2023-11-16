d3373dccb0b01528b2b75a02e1df51199d987493
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 02:51:43 -0400

    fixed error in printing (nil)

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,16 +14,16 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("%s\n", d->name);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 
 		if (d->age != 0)
 			printf("%f\n", d->age);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 
 		if (d->owner != NULL)
 			printf("%s\n", d->owner);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 	}
 }
