7b12e56e398d25378a5c8ed4566fb1e393d53609
leomar <leomar.rodriguez1@upr.edu>
2023-11-17 17:49:57 -0400

    finished with all the betty errors

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,31 +3,51 @@
 
 /**
  * op_add - adds two ints
- * op_sub - substracts two ints
- * op_mul - multiplies two ints
- * op_div - divides two ints
- * op_mod - gets the remainder of a div
  * @a: num1
  * @b: num2
- * Returns: returns the result of the op required
+ * Return: returns the result of the op required
 */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - substract two ints
+ * @a: num1
+ * @b: num2
+ * Return: returns the result of the op
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - multiplies two ints
+ * @a: num1
+ * @b: num2
+ * Return: returns the result of the op
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - divides two ints
+ * @a: num1
+ * @b: num2
+ * Return: returns the result of the op
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - gets remainder of two ints
+ * @a: num1
+ * @b: num2
+ * Return: returns the result of the op
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
