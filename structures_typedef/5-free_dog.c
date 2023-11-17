fe9ffc78ee70a64e20eff6db1a7ed3239c4f9350
leomar <leomar.rodriguez1@upr.edu>
2023-11-16 22:44:54 -0400

    funciton that frees a dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees allocated memory
+ * @d: the dog to free
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
