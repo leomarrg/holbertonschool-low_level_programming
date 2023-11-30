43cc545add628cb500b945aeef0059a9021c5b99
leomar <leomar.rodriguez1@upr.edu>
2023-11-30 18:53:58 -0400

    function that frees a linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function that frees linked lists
+ * @head: head mode
+ * Return: returns nothing
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmpHead;
+	while (head != NULL)
+	{
+		tmpHead = head;
+		head = head->next;
+		free(tmpHead);
+	}
+}
