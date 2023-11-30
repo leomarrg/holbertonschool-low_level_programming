cdca18605632937e5a10e2bc7f0cec74e4446127
leomar <leomar.rodriguez1@upr.edu>
2023-11-30 16:38:05 -0400

    fixed errors in betty on lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,12 +13,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
-
-
 
 size_t print_list(const list_t *h);
 int _putchat(char c);
