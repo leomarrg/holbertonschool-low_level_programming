fc9de01a47e101bc6509072dd28669d947564ec4
leomar <leomar.rodriguez1@upr.edu>
2023-12-03 15:02:10 -0400

    deleted conditions that inserted at the end to test output handling out of index edge cases

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,9 +27,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	}
 
-	if (currNode->next == NULL && idx > 0)
-		return (add_dnodeint_end(h, n));
-
 
 	for (index = 0; index < idx - 1 && currNode != NULL; index++)
 	{
@@ -38,12 +35,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	if (idx >= index + 2)
-		return (add_dnodeint_end(h, n));
-
 	newNode->prev = currNode;
 	newNode->next = currNode->next;
-	currNode->next->prev = newNode;
+	if (currNode->next != NULL)
+		currNode->next->prev = newNode;
 	currNode->next = newNode;
 
 	return (newNode);
