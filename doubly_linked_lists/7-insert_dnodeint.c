2bfa0290a06fb47d5ed25e73a2cdc10c7aaef1aa
leomar <leomar.rodriguez1@upr.edu>
2023-12-03 13:05:04 -0400

    function that adds a node at the nth index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - function to insert a new node at a given position
+ * @h: head node
+ * @idx: index node
+ * @n:data received
+ * Return: returns address of new node
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *currNode, *tmpNode;
+	unsigned int index;
+
+	index = 0;
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	currNode = *h;
+
+	if (newNode == NULL || currNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (idx == 0)
+		add_dnodeint(h, n);
+
+	if (currNode->next == NULL)
+		add_dnodeint_end(h, n);
+
+	while (currNode != NULL)
+	{
+		currNode = currNode->next;
+		index++;
+
+		if (index == (idx - 1))
+		{
+			tmpNode = currNode->next;
+			newNode->next = currNode->next;
+			newNode->prev = currNode;
+			currNode->next = newNode;
+			tmpNode->prev = newNode;
+		}
+	}
+
+	return (tmpNode);
+}
