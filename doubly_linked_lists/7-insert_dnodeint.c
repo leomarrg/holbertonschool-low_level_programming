3e62d6cabaa01fa106eea48c9fa008b56706b340
leomar <leomar.rodriguez1@upr.edu>
2023-12-03 13:48:57 -0400

    changed conditions to verify if index is out of bounds

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,13 +14,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newNode, *currNode, *tmpNode;
 	unsigned int index;
 
-	index = 0;
 	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
 	currNode = *h;
 
 	if (newNode == NULL)
 		return (NULL);
 
+	newNode->prev = NULL;
 	newNode->n = n;
 	newNode->next = NULL;
 
@@ -30,25 +30,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (currNode->next == NULL)
-		add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 
-	while (currNode != NULL)
+	if (currNode == NULL)
 	{
-		if (index == idx - 1)
-		{
-			tmpNode = currNode->next;
-			newNode->next = currNode->next;
-			newNode->prev = currNode;
-			currNode->next = newNode;
-			if (tmpNode != NULL)
-				tmpNode->prev = newNode;
-			if (idx == 0)
-				*h = newNode;
-
-			break;
-		}
-		currNode = currNode->next;
-		index++;
+		free(newNode);
+		return (NULL);
 	}
-	return (tmpNode);
+
+	for (index = 0; index < idx - 1 && currNode != NULL; index++)
+	{
+		currNode = currNode->next;
+	}
+
+	tmpNode = currNode->next;
+	currNode->next = newNode;
+	newNode->prev = currNode;
+	newNode->next = tmpNode;
+
+	if (tmpNode != NULL)
+		tmpNode->prev = newNode;
+
+	return (newNode);
 }
