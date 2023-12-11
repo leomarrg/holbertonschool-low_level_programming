b26ed38abaef7734a8428a59632ce11f6a64b860
leomar <leomar.rodriguez1@upr.edu>
2023-12-10 22:15:39 -0400

    fixing error in betty styler in 0-hash_tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,28 +11,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
-
 	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
-
 	if (ht == NULL)
 		return (NULL);
-
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
-	
-
 	for (i = 0; i < size; i++)
 	{
 		ht->array[i] = NULL;
 	}
-
 	free(ht);
-
 	return (ht);
 }
