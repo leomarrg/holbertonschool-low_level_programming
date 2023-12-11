08d314711bd295b661b9a3638718989ac54e5c08
leomar <leomar.rodriguez1@upr.edu>
2023-12-10 22:16:41 -0400

    fixed error in betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
+
+
 	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
