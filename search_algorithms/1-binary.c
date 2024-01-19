c4c17547055b3c678e2c9a5dda8029acf5c753e7
leomar <leomar.rodriguez1@upr.edu>
2024-01-19 00:48:43 -0400

    pushing binary search file

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - implements binary search algo
+ * @array: pointer to the first element
+ * @size: size of the array
+ * @value: value searched for
+ * Return: returns index of value or -1 if array null or value not found
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, low, mid, high;
+
+	low = 0;
+	high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+		{
+			printf("Found %d at index: %ld\n", value, mid);
+			return mid;
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+
+	printf("Found %d at index: -1\n", value);
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
